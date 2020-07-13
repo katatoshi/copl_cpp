7600fe2881ab168a28e1fd5315eb376b60f7bea3
Toshifumi Katayama <kata.toshi@gmail.com>
2020-07-14 02:25:55 +0900

    ユニットテストで create_xxx_exp 関数を使って式を構築するように変更
    ml_driver_test.cpp, ml_interpreter_test.cpp での式の構築を
    create_xxx_exp で行うように変更した。

--- test/ml_driver_test.cpp
+++ test/ml_driver_test.cpp
@@ -51,14 +51,10 @@ TEST_F(MLDriverTest, TestParseBoolExp) {
 }
 
 TEST_F(MLDriverTest, TestParsePlusOpExp) {
-    IntExp int_exp_left = 3;
-    auto exp_left = std::make_shared<const Exp>(int_exp_left);
-
-    IntExp int_exp_right = -5;
-    auto exp_right = std::make_shared<const Exp>(int_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Plus(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
+    auto expected_exp = create_plus_op_exp(
+        create_int_exp(3),
+        create_int_exp(-5)
+    );
 
     std::istringstream is("3 + -5 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -71,14 +67,10 @@ TEST_F(MLDriverTest, TestParsePlusOpExp) {
 }
 
 TEST_F(MLDriverTest, TestParseMinusOpExp) {
-    IntExp int_exp_left = 2;
-    auto exp_left = std::make_shared<const Exp>(int_exp_left);
-
-    IntExp int_exp_right = -11;
-    auto exp_right = std::make_shared<const Exp>(int_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Minus(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
+    auto expected_exp = create_minus_op_exp(
+        create_int_exp(2),
+        create_int_exp(-11)
+    );
 
     std::istringstream is("2 - -11 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -91,14 +83,10 @@ TEST_F(MLDriverTest, TestParseMinusOpExp) {
 }
 
 TEST_F(MLDriverTest, TestParseTimesOpExp) {
-    IntExp int_exp_left = 3;
-    auto exp_left = std::make_shared<const Exp>(int_exp_left);
-
-    IntExp int_exp_right = 9;
-    auto exp_right = std::make_shared<const Exp>(int_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Times(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
+    auto expected_exp = create_times_op_exp(
+        create_int_exp(3),
+        create_int_exp(9)
+    );
 
     std::istringstream is("3 * 9 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -110,15 +98,11 @@ TEST_F(MLDriverTest, TestParseTimesOpExp) {
     EXPECT_EQ(exp_to_string(*expected_exp), exp_to_string(*actual_exp));
 }
 
-TEST_F(MLDriverTest, TestParseLtOpExpTrue) {
-    IntExp int_exp_left = 1;
-    auto exp_left = std::make_shared<const Exp>(int_exp_left);
-
-    IntExp int_exp_right = 4;
-    auto exp_right = std::make_shared<const Exp>(int_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Lt(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
+TEST_F(MLDriverTest, TestParseLtOpExp) {
+    auto expected_exp = create_lt_op_exp(
+        create_int_exp(1),
+        create_int_exp(4)
+    );
 
     std::istringstream is("1 < 4 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -130,61 +114,12 @@ TEST_F(MLDriverTest, TestParseLtOpExpTrue) {
     EXPECT_EQ(exp_to_string(*expected_exp), exp_to_string(*actual_exp));
 }
 
-TEST_F(MLDriverTest, TestParseLtOpExpFalse) {
-    IntExp int_exp_left = -7;
-    auto exp_left = std::make_shared<const Exp>(int_exp_left);
-
-    IntExp int_exp_right = -11;
-    auto exp_right = std::make_shared<const Exp>(int_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Lt(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
-
-    std::istringstream is("-7 < -11 ;;\n");
-    EXPECT_EQ(true, driver.parse(is));
-
-    auto optional_parsed_exp = driver.get_parsed_exp();
-    EXPECT_EQ(true, optional_parsed_exp.has_value());
-
-    auto actual_exp = *optional_parsed_exp;
-    EXPECT_EQ(exp_to_string(*expected_exp), exp_to_string(*actual_exp));
-}
-
-TEST_F(MLDriverTest, TestParseIfExpTrue) {
-    BoolExp bool_exp_cond = true;
-    auto exp_cond = std::make_shared<const Exp>(bool_exp_cond);
-
-    IntExp int_exp_true = 9;
-    auto exp_true = std::make_shared<const Exp>(int_exp_true);
-
-    IntExp int_exp_false = 3;
-    auto exp_false = std::make_shared<const Exp>(int_exp_false);
-
-    auto if_exp = std::make_shared<const IfExp>(exp_cond, exp_true, exp_false);
-    auto expected_exp = std::make_shared<const Exp>(if_exp);
-
-    std::istringstream is("if true then 9 else 3 ;;\n");
-    EXPECT_EQ(true, driver.parse(is));
-
-    auto optional_parsed_exp = driver.get_parsed_exp();
-    EXPECT_EQ(true, optional_parsed_exp.has_value());
-
-    auto actual_exp = *optional_parsed_exp;
-    EXPECT_EQ(exp_to_string(*expected_exp), exp_to_string(*actual_exp));
-}
-
-TEST_F(MLDriverTest, TestParseIfExpFalse) {
-    BoolExp bool_exp_cond = false;
-    auto exp_cond = std::make_shared<const Exp>(bool_exp_cond);
-
-    IntExp int_exp_true = 5;
-    auto exp_true = std::make_shared<const Exp>(int_exp_true);
-
-    IntExp int_exp_false = 13;
-    auto exp_false = std::make_shared<const Exp>(int_exp_false);
-
-    auto if_exp = std::make_shared<const IfExp>(exp_cond, exp_true, exp_false);
-    auto expected_exp = std::make_shared<const Exp>(if_exp);
+TEST_F(MLDriverTest, TestParseIfExp) {
+    auto expected_exp = create_if_exp(
+        create_bool_exp(false),
+        create_int_exp(5),
+        create_int_exp(13)
+    );
 
     std::istringstream is("if false then 5 else 13 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -197,29 +132,17 @@ TEST_F(MLDriverTest, TestParseIfExpFalse) {
 }
 
 TEST_F(MLDriverTest, TestParseStrangeFormatExp) {
-    IntExp int_exp_cond_left = -11;
-    auto exp_cond_left = std::make_shared<const Exp>(int_exp_cond_left);
-
-    IntExp int_exp_cond_right = 0;
-    auto exp_cond_right = std::make_shared<const Exp>(int_exp_cond_right);
-
-    auto op_exp_cond = std::make_shared<const OpExp>(exp_cond_left, Lt(), exp_cond_right);
-    auto exp_cond = std::make_shared<const Exp>(op_exp_cond);
-
-    IntExp int_exp_true_left = -3;
-    auto exp_true_left = std::make_shared<const Exp>(int_exp_true_left);
-
-    IntExp int_exp_true_right = 7;
-    auto exp_true_right = std::make_shared<const Exp>(int_exp_true_right);
-
-    auto op_exp_true = std::make_shared<const OpExp>(exp_true_left, Times(), exp_true_right);
-    auto exp_true = std::make_shared<const Exp>(op_exp_true);
-
-    IntExp int_exp_false = -3;
-    auto exp_false = std::make_shared<const Exp>(int_exp_false);
-
-    auto if_exp = std::make_shared<const IfExp>(exp_cond, exp_true, exp_false);
-    auto expected_exp = std::make_shared<const Exp>(if_exp);
+    auto expected_exp = create_if_exp(
+        create_lt_op_exp(
+            create_int_exp(-11),
+            create_int_exp(0)
+        ),
+        create_times_op_exp(
+            create_int_exp(-3),
+            create_int_exp(7)
+        ),
+        create_int_exp(-3)
+    );
 
     std::istringstream is(
 R"((
@@ -239,20 +162,13 @@ if((-11)<0)
 }
 
 TEST_F(MLDriverTest, Test_Parse_LP_3_PLUS_MINUS_6_RP_TIMES_5_IS_MINUS_15) {
-    IntExp int_exp_left_left = 3;
-    auto exp_left_left = std::make_shared<const Exp>(int_exp_left_left);
-
-    IntExp int_exp_left_right = -6;
-    auto exp_left_right = std::make_shared<const Exp>(int_exp_left_right);
-
-    auto op_exp_left = std::make_shared<const OpExp>(exp_left_left, Plus(), exp_left_right);
-    auto exp_left = std::make_shared<const Exp>(op_exp_left);
-
-    IntExp int_exp_right = 5;
-    auto exp_right = std::make_shared<const Exp>(int_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Times(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
+    auto expected_exp = create_times_op_exp(
+        create_plus_op_exp(
+            create_int_exp(3),
+            create_int_exp(-6)
+        ),
+        create_int_exp(5)
+    );
 
     std::istringstream is("(3 + -6) * 5 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -265,20 +181,13 @@ TEST_F(MLDriverTest, Test_Parse_LP_3_PLUS_MINUS_6_RP_TIMES_5_IS_MINUS_15) {
 }
 
 TEST_F(MLDriverTest, Test_Parse_13_LT_LP_17_MINUS_4_RP_IS_FALSE) {
-    IntExp int_exp_left = 13;
-    auto exp_left = std::make_shared<const Exp>(int_exp_left);
-
-    IntExp int_exp_right_left = 17;
-    auto exp_right_left = std::make_shared<const Exp>(int_exp_right_left);
-
-    IntExp int_exp_right_right = 4;
-    auto exp_right_right = std::make_shared<const Exp>(int_exp_right_right);
-
-    auto op_exp_right = std::make_shared<const OpExp>(exp_right_left, Minus(), exp_right_right);
-    auto exp_right = std::make_shared<const Exp>(op_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Lt(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
+    auto expected_exp = create_lt_op_exp(
+        create_int_exp(13),
+        create_minus_op_exp(
+            create_int_exp(17),
+            create_int_exp(4)
+        )
+    );
 
     std::istringstream is("13 < (17 - 4) ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -291,29 +200,17 @@ TEST_F(MLDriverTest, Test_Parse_13_LT_LP_17_MINUS_4_RP_IS_FALSE) {
 }
 
 TEST_F(MLDriverTest, Test_Parse_IF_3_LT_7_THEN_MINUS_7_TIMES_MINUS_4_ELSE_2_IS_28) {
-    IntExp int_exp_cond_left = 3;
-    auto exp_cond_left = std::make_shared<const Exp>(int_exp_cond_left);
-
-    IntExp int_exp_cond_right = 7;
-    auto exp_cond_right = std::make_shared<const Exp>(int_exp_cond_right);
-
-    auto op_exp_cond = std::make_shared<const OpExp>(exp_cond_left, Lt(), exp_cond_right);
-    auto exp_cond = std::make_shared<const Exp>(op_exp_cond);
-
-    IntExp int_exp_true_left = -7;
-    auto exp_true_left = std::make_shared<const Exp>(int_exp_true_left);
-
-    IntExp int_exp_true_right = -4;
-    auto exp_true_right = std::make_shared<const Exp>(int_exp_true_right);
-
-    auto op_exp_true = std::make_shared<const OpExp>(exp_true_left, Times(), exp_true_right);
-    auto exp_true = std::make_shared<const Exp>(op_exp_true);
-
-    IntExp int_exp_false = 2;
-    auto exp_false = std::make_shared<const Exp>(int_exp_false);
-
-    auto if_exp = std::make_shared<const IfExp>(exp_cond, exp_true, exp_false);
-    auto expected_exp = std::make_shared<const Exp>(if_exp);
+    auto expected_exp = create_if_exp(
+        create_lt_op_exp(
+            create_int_exp(3),
+            create_int_exp(7)
+        ),
+        create_times_op_exp(
+            create_int_exp(-7),
+            create_int_exp(-4)
+        ),
+        create_int_exp(2)
+    );
 
     std::istringstream is("if 3 < 7 then -7 * -4 else 2 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
@@ -326,27 +223,14 @@ TEST_F(MLDriverTest, Test_Parse_IF_3_LT_7_THEN_MINUS_7_TIMES_MINUS_4_ELSE_2_IS_2
 }
 
 TEST_F(MLDriverTest, Test_Parse_LP_IF_FALSE_THEN_TRUE_ELSE_11_RP_PLUS_MINUS_8_IS_3) {
-    BoolExp bool_exp_left_cond = false;
-    auto exp_left_cond = std::make_shared<const Exp>(bool_exp_left_cond);
-
-    BoolExp bool_exp_left_true = true;
-    auto exp_left_true = std::make_shared<const Exp>(bool_exp_left_true);
-
-    IntExp int_exp_left_false = 11;
-    auto exp_left_false = std::make_shared<const Exp>(int_exp_left_false);
-
-    auto if_exp_left = std::make_shared<const IfExp>(
-        exp_left_cond,
-        exp_left_true,
-        exp_left_false
+    auto expected_exp = create_plus_op_exp(
+        create_if_exp(
+            create_bool_exp(false),
+            create_bool_exp(true),
+            create_int_exp(11)
+        ),
+        create_int_exp(-8)
     );
-    auto exp_left = std::make_shared<const Exp>(if_exp_left);
-
-    IntExp int_exp_right = -8;
-    auto exp_right = std::make_shared<const Exp>(int_exp_right);
-
-    auto op_exp = std::make_shared<const OpExp>(exp_left, Plus(), exp_right);
-    auto expected_exp = std::make_shared<const Exp>(op_exp);
 
     std::istringstream is("(if false then true else 11) + -8 ;;\n");
     EXPECT_EQ(true, driver.parse(is));
